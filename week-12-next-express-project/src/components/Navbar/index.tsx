045972693d6242673f1b0fc45341df076171f818
imanmaris99 <imanmaris99@yahoo.com>
2024-01-19 22:08:24 +0700

    try to build with SSR

--- week-12-next-express-project/src/components/Navbar/index.tsx
+++ week-12-next-express-project/src/components/Navbar/index.tsx
@@ -34,7 +34,7 @@ const Navbar = () => {
                     <div className="flex space-x-4">
                         <Link passHref href={'/'} className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">{'Home'}</Link>
                         <Link passHref href={'/weather/register'} className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">{'Registrasi'}</Link>
-                        <Link passHref href={'/weather/weatherFinder'} className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">{'Weather Finder'}</Link>
+                        <Link passHref href={'/weather'} className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">{'Weather Finder'}</Link>
                     </div>
                     </div>
                 </div>               
