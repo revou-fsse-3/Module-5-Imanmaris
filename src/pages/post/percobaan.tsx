80ee4381f5c2c67b17ed510ecfc5a81cf7f13963
imanmaris99 <imanmaris99@yahoo.com>
2024-01-21 17:43:01 +0700

    try to created pokemonlist page with SSR

---
+++ src/pages/post/percobaan.tsx
@@ -0,0 +1,69 @@
+import { Card, OldCard } from "@/components";
+import Layout from "@/layouts";
+
+interface Post {
+    title: string;
+  }
+  
+  interface Resulted {
+    name: string;
+    url:string;
+  }
+  
+  interface ListPokemon {
+    results: Resulted[];
+  }
+  
+  interface Props {
+    posts: Post[];
+    results: ListPokemon;
+  }
+  
+  const CobaPost = ({ results }: Props) => {
+    return (
+      
+      <main 
+      className={`bg-sky-50/[.9] rounded-b-xl flex min-h-screen flex-col items-center justify-center`}
+      >
+        <Layout>
+          <OldCard direction="column">
+            <section className="w-full p-5 bg-sky-100/[.9] text-black flex flex-col justify-center rounded-md">
+              <h2 className="w-full p-5 text-3xl bg-sky-100/[.9] text-black flex justify-center rounded-md">
+                {'Halaman Pokemon Post'}
+                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                  -&gt;
+                </span>
+              </h2>
+            <OldCard direction="column">
+              {results.results.map((result, index) => (
+                
+                <OldCard key={index} direction="column" display="flex" justifyContent="center">
+                  <p key={index}>{result.name}</p>
+                  <p key={index}>{result.url}</p>               
+                </OldCard>
+
+              ))}
+            </OldCard>
+
+            </section>
+          </OldCard>
+        </Layout>
+
+      </main>
+    );
+  };
+  
+  export const getServerSideProps = async () => {
+    const responseKedua = await fetch('https://pokeapi.co/api/v2/pokemon?limit=15');
+    const timedata: ListPokemon = await responseKedua.json();
+    console.log (timedata);
+
+    return {
+      props: {
+        results: timedata,
+      },
+    };
+  };
+  
+  export default CobaPost;
+  
--- src/pages/weather/register.tsx
+++ src/pages/weather/register.tsx
@@ -4,7 +4,8 @@ import { Card } from "@/components";
 import DateUpdate from "@/components/Assets/DateUpdate";
 import {useFormik} from "formik";
 import * as yup from"yup";
-import router from "next/router";
+import {useRouter} from "next/router";
+import Link from "next/link";
 // import Link from "next/link";
 
 // Define an interface for the form data
@@ -14,6 +15,8 @@ interface FormData {
   }
 
 const Register = () => {
+
+    const router = useRouter();
 
     const generateCustomToken = () => {
         // Customize this function to generate your custom token logic
@@ -31,11 +34,13 @@ const Register = () => {
           email: "",
           password: "",
         },
-        onSubmit: (values, {resetForm}) => {
+        onSubmit: async (values, {resetForm}) => {
           // Handle form submission logic here
           console.log("Form values submitted:", values)
 
           try {
+            await forMik.validateForm();
+            
             // Generate a custom token locally
             const customToken = generateCustomToken();
     
@@ -56,82 +61,74 @@ const Register = () => {
 
         validationSchema: yup.object({
           email: yup.string().email('invalid email format, example => agus@example.com').required('Email is required'),
-          password: yup.string().required(),            
+          password: yup.string().required('Password is required'),            
         })
 
       });
 
-        
-      
-      
+
+      return (
+        <main className={`bg-sky-50/[.9] rounded-b-xl flex min-h-screen flex-col items-center justify-center`}>
+          <Layout>
+            <section className="w-full p-5 bg-sky-100/[.9] text-black flex flex-col justify-center rounded-md">
+              <h1 className="w-full p-5 text-3xl bg-sky-100/[.9] text-black flex justify-center rounded-md">REGISTER HERE</h1>
+    
+              <div className="text-xl m-5 flex justify-center">
+                <DateUpdate />
+              </div>
+    
+              <Card>
+                <form onSubmit={forMik.handleSubmit}>
+                  <div className="w-full p-5 text-2xl bg-sky-100/[.9] text-black flex flex-col justify-center rounded-md">
+                    <label>Email</label>
+                    <input
+                      className="border-solid border-2 border-sky-500 py-1"
+                      name="email"
+                      value={forMik.values.email}
+                      onBlur={forMik.handleBlur("email")}
+                      onChange={forMik.handleChange("email")}
+                    />
+                    {forMik.errors.email && (
+                      <div className="text-red-500">{forMik.errors.email}</div>
+                    )}
+                  </div>
+    
+                  <div className="w-full p-5 text-2xl bg-sky-100/[.9] text-black flex flex-col justify-center rounded-md">
+                    <label>Password</label>
+                    <input
+                      type="password"
+                      className="border-solid border-2 border-sky-500 py-1"
+                      name="password"
+                      value={forMik.values.password}
+                      onBlur={forMik.handleBlur("password")}
+                      onChange={forMik.handleChange("password")}
+                    />
+                    {forMik.errors.password && (
+                      <div className="text-red-500">{forMik.errors.password}</div>
+                    )}
+                  </div>
+    
 
 
-  return (
+                  <section className={'flex flex-wrap flex-col items-center mt-2'}>
+                  <button
+                    type="submit"
+                    className="w-2/3 mt-3 mb-4 text-2xl bg-green-400 text-gray-100 hover:bg-green-700 hover:text-white rounded-lg py-2 font-medium"
+                  >
+                    {'Register to Weather Finder'}
+                  </button>
 
-    <main 
-        className={`bg-sky-50/[.9] rounded-b-xl flex min-h-screen flex-col items-center justify-center`}
-        >
-            <Layout>
-                <section className="w-full p-5 bg-sky-100/[.9] text-black flex flex-col justify-center rounded-md">
-                    <h1 className="w-full p-5 text-3xl bg-sky-100/[.9] text-black flex justify-center rounded-md">REGISTER HERE</h1>
+                    <Link passHref href={'/post/percobaan'} className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-lg m-2 px-3 py-2 text-2xl font-medium">{'Goes to Pokemon List'}</Link>
+                  </section>
 
-                    <div className="text-xl m-5 flex justify-center">
-                        <DateUpdate/>
-                    </div>
-
-                        <Card>
-                            <form onSubmit={forMik.handleSubmit} >
-                                <div className="w-full p-5 text-2xl bg-sky-100/[.9] text-black flex flex-col justify-center rounded-md">
-                                <label>Email</label>
-                                <input
-                                    className="border-solid border-2 border-sky-500 py-1"
-                                    name="email"
-                                    value={forMik.values.email}
-                                    onBlur={forMik.handleBlur("email")}
-                                    onChange={forMik.handleChange("email")}
-                                />
-                                {
-                                  forMik.errors.email && (
-                                    <text>{forMik.errors.email}</text>
-                                  )
-                                  
-                                  }
-                                </div>
-
-                                <div className="w-full p-5 text-2xl bg-sky-100/[.9] text-black flex flex-col justify-center rounded-md">
-                                <label>Password</label>
-                                <input
-                                    type="password"
-                                    className="border-solid border-2 border-sky-500 py-1"
-                                    name="password"
-                                    value={forMik.values.password}
-                                    onBlur={forMik.handleBlur("password")}
-                                    onChange={forMik.handleChange("password")}
-                                />
-                                {
-                                  forMik.errors.password && (
-                                    <text>{forMik.errors.password}</text>
-                                  )
-                                  
-                                  }                                
-                                </div>
-
-                                <button
-                                type={"submit"}
-                                className="w-full mt-3 mb-2 text-2xl bg-green-400 text-gray-100 hover:bg-green-700 hover:text-white rounded-md py-2 font-medium"
-                                >
-                                {'Register'}
-                                </button>
-                            </form>
-                        </Card>
-
-                </section>
-            </Layout>
-    </main>
-
-  )
-}
-
-export default Register
+                </form>
+              </Card>
+            </section>
+          </Layout>
+        </main>
+      );
+    };
+    
+    export default Register;
 
 
